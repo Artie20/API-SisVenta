4473bb252f1ec68667463d844223e2c2f1025028
Artie20 <149128422+Artie20@users.noreply.github.com>
2025-11-18 17:26:41 -0600

    Add files via upload

---
+++ API-SisVenta/Controllers/ClienteController.cs
@@ -0,0 +1,66 @@
+﻿using API_Cliente.CasosDeUso;
+using API_SisVenta.Dtos;
+using API_SisVenta.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_SisVenta.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClienteController : Controller
+    {
+        private readonly DBVENTAbakContext _DBventabakcontext;
+        private readonly IActualizaCasoDeUso _actualizaCasoDeUso;
+
+        public ClienteController(DBVENTAbakContext DBventabakcontext)
+        {
+            _DBventabakcontext = DBventabakcontext;
+        }
+
+        [HttpGet()]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<ClienteDto>))]
+        [HttpGet]
+        public IActionResult TraeClientes()
+        {
+            var result = _DBventabakcontext.Cliente.Select(c =>c.ToDto()).ToList();
+            return new OkObjectResult(result);
+        }
+
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ClienteDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> TraeCliente(int id)
+        {
+            ClienteEntity result = await _DBventabakcontext.Get(id);
+            return new OkObjectResult(result.ToDto());
+
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        public async Task<IActionResult> EliminaCliente(int id)
+        {
+            var result = await _DBventabakcontext.Delete(id);
+            return new OkObjectResult(result);
+        }
+
+        [HttpPost()]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(ClienteDto))]
+        public async Task<IActionResult> CreaCliente(CreaClienteDto clientes)
+
+        {
+            ClienteEntity result = await _DBventabakcontext.Add(clientes);
+            return new CreatedResult($"http://localhost:5181/api/clientes/{result.idCliente}", null);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(ClienteDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ClienteDto> ActualizaCliente(ClienteDto clientes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
