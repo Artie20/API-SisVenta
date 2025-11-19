35518cc107a97473457502d8802c3a7dad67a073
Artie20 <149128422+Artie20@users.noreply.github.com>
2025-11-19 16:31:40 -0600

    Add files via upload

---
+++ API-SisVenta/CasosDeUso/ActualizaRolCasoDeUso.cs
@@ -0,0 +1,6 @@
+﻿namespace API_SisVenta.CasosDeUso
+{
+    public class ActualizaRolCasoDeUso
+    {
+    }
+}
---
+++ API-SisVenta/CasosDeUso/ActualizaUsuarioCasoDeUso.cs
@@ -0,0 +1,39 @@
+﻿using API_SisVenta.Dtos;
+using API_SisVenta.Repositories;
+
+namespace API_Cliente.CasosDeUso
+{
+    public interface IActualizaUsuarioCasoDeUso
+    {
+        Task<UsuarioDto?> Execute(ActualizarUsuarioDto dto);
+    }
+
+    public class ActualizaUsuarioCasoDeUso : IActualizaUsuarioCasoDeUso
+    {
+        private readonly DBVENTAbakContext _context;
+
+        public ActualizaUsuarioCasoDeUso(DBVENTAbakContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UsuarioDto?> Execute(ActualizarUsuarioDto dto)
+        {
+            var entity = await _context.GetUsuario(dto.idUsuario);
+
+            if (entity == null)
+                return null;
+
+            entity.nombre = dto.nombre;
+            entity.correo = dto.correo;
+            entity.telefono = dto.telefono;
+            entity.idRol = dto.idRol;
+            entity.esActivo = dto.esActivo;
+
+            await _context.ActualizarUsuario(entity);
+
+            return entity.ToDto();
+        }
+    }
+}
+
