17501018e0e98706e8f0c8a39c575b50bf447eaa
Artie20 <149128422+Artie20@users.noreply.github.com>
2025-11-19 16:32:25 -0600

    Add files via upload

---
+++ API-SisVenta/Controllers/RolController.cs
@@ -0,0 +1,68 @@
+﻿using API_SisVenta.Dtos;
+using API_SisVenta.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_SisVenta.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RolController : Controller
+    {
+        private readonly DBVENTAbakContext _context;
+
+        public RolController(DBVENTAbakContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<RolDto>))]
+        public IActionResult TraeRoles()
+        {
+            var result = _context.Rol
+
+                                 .Select(r => r.ToDto())
+                                 .ToList();
+
+            return new OkObjectResult(result);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(RolDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> TraeRol(int id)
+        {
+            var entity = await _context.Rol.FindAsync(id);
+
+            if (entity == null)
+                return new NotFoundResult();
+
+            return new OkObjectResult(entity.ToDto());
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(RolDto))]
+        public async Task<IActionResult> CreaRol(CreaRolDto dto)
+        {
+            var entity = await _context.AddRol(dto);
+            return new CreatedResult($"http://localhost:5181/api/rol/{entity.IdRol}", null);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(RolDto))]
+        public async Task<IActionResult> ActualizaRol(RolDto dto)
+        {
+            var entity = await _context.UpdateRol(dto);
+            return new OkObjectResult(entity);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> EliminaRol(int id)
+        {
+            bool result = await _context.DeleteRol(id);
+            return new OkObjectResult(result);
+        }
+    }
+}
+
---
+++ API-SisVenta/Controllers/UsuarioController.cs
@@ -0,0 +1,86 @@
+﻿using API_Cliente.CasosDeUso;
+using API_SisVenta.Dtos;
+using API_SisVenta.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static API_SisVenta.Repositories.DBVENTAbakContext;
+
+namespace API_SisVenta.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : Controller
+    {
+        private readonly DBVENTAbakContext _context;
+        private readonly IActualizaUsuarioCasoDeUso _actualizaUsuarioCasoDeUso;
+
+        public UsuarioController(DBVENTAbakContext context, IActualizaUsuarioCasoDeUso actualizaUsuarioCasoDeUso)
+        {
+            _context = context;
+            _actualizaUsuarioCasoDeUso = actualizaUsuarioCasoDeUso;
+        }
+
+        // GET: api/usuario
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<UsuarioDto>))]
+        public IActionResult TraeUsuarios()
+        {
+            var result = _context.Usuario
+                                 .Include(u => u.Rol)
+                                 .Select(u => u.ToDto())
+                                 .ToList();
+
+            return new OkObjectResult(result);
+        }
+
+        // GET: api/usuario/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UsuarioDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> TraeUsuario(int id)
+        {
+            var result = await _context.Usuario
+                                       .Include(u => u.Rol)
+                                       .FirstOrDefaultAsync(u => u.IdUsuario == id);
+
+            if (result == null)
+                return new NotFoundResult();
+
+            return new OkObjectResult(result.ToDto());
+        }
+
+        // POST: api/usuario
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(UsuarioDto))]
+        public async Task<IActionResult> CreaUsuario(CreaUsuarioDto dto)
+        {
+            UsuarioEntity entity = await _context.AddUsuario(dto);
+
+            return new CreatedResult($"http://localhost:5181/api/usuario/{entity.IdUsuario}", null);
+        }
+
+        // DELETE: api/usuario/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        public async Task<IActionResult> EliminaUsuario(int id)
+        {
+            bool result = await _context.DeleteUsuario(id);
+            return new OkObjectResult(result);
+        }
+
+        // PUT: api/usuario
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(UsuarioDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> ActualizaUsuario(UsuarioDto usuario)
+        {
+            UsuarioDto? result = await _actualizaUsuarioCasoDeUso.Execute(usuario);
+
+            if (result == null)
+                return new NotFoundResult();
+
+            return new OkObjectResult(result);
+        }
+    }
+}
+
