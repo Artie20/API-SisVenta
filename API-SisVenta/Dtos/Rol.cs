e192ae9a498e783c0d2e3f8b58025d0d8db401c3
Artie20 <149128422+Artie20@users.noreply.github.com>
2025-11-19 16:33:33 -0600

    Add files via upload

---
+++ API-SisVenta/Dtos/Rol.cs
@@ -0,0 +1,17 @@
+﻿namespace API_SisVenta.Dtos
+{
+    public class RolDto
+    {
+        public int IdRol { get; set; }
+        public string? Descripcion { get; set; }
+        public bool? EsActivo { get; set; }
+    }
+
+    public class CreaRolDto
+    {
+        public string? Descripcion { get; set; }
+        public bool? EsActivo { get; set; }
+    }
+
+
+}
---
+++ API-SisVenta/Dtos/Usuario.cs
@@ -0,0 +1,35 @@
+﻿namespace API_SisVenta.Dtos
+{
+    public class UsuarioDto
+    {
+        public int idUsuario { get; set; }
+        public string nombre { get; set; }
+        public string correo { get; set; }
+        public string telefono { get; set; }
+        public int? idRol { get; set; }
+        public string urlFoto { get; set; }
+        public string nombreFoto { get; set; }
+        public bool esActivo { get; set; }
+        public DateTime fechaRegistro { get; set; }
+    }
+
+    public class CrearUsuarioDto
+    {
+        public string nombre { get; set; }
+        public string correo { get; set; }
+        public string telefono { get; set; }
+        public int? idRol { get; set; }
+        public string clave { get; set; }
+    }
+
+    public class ActualizarUsuarioDto
+    {
+        public int idUsuario { get; set; }
+        public string nombre { get; set; }
+        public string correo { get; set; }
+        public string telefono { get; set; }
+        public int? idRol { get; set; }
+        public bool esActivo { get; set; }
+    }
+}
+
