e708a4a44db7fe4f0844f0aed449ddd9b8e1029e
Artie20 <149128422+Artie20@users.noreply.github.com>
2025-11-20 17:18:19 -0600

    Add methods for Rol entity management

--- API-SisVenta/Repositories/DBVENTAbakContext.cs
+++ API-SisVenta/Repositories/DBVENTAbakContext.cs
@@ -1,6 +1,7 @@
 using API_SisVenta.Dtos;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace API_SisVenta.Repositories
 {
@@ -19,15 +20,15 @@ namespace API_SisVenta.Repositories
                 .HasKey(c => c.idCliente);
 
             modelBuilder.Entity<UsuarioEntity>()
-                .HasKey(u => u.IdUsuario);
+                .HasKey(u => u.idUsuario);
 
             modelBuilder.Entity<RolEntity>()
-                .HasKey(r => r.IdRol);
+                .HasKey(r => r.idRol);
 
             modelBuilder.Entity<UsuarioEntity>()
                 .HasOne(u => u.Rol)
                 .WithMany(r => r.Usuarios)
-                .HasForeignKey(u => u.IdRol);
+                .HasForeignKey(u => u.idRol);
 
             base.OnModelCreating(modelBuilder);
         }
@@ -73,19 +74,19 @@ namespace API_SisVenta.Repositories
 
         // Métodos Usuario
         public async Task<UsuarioEntity?> GetUsuario(int id) =>
-            await Usuario.FirstOrDefaultAsync(x => x.IdUsuario == id);
+            await Usuario.FirstOrDefaultAsync(x => x.idUsuario == id);
 
         public async Task<UsuarioEntity> AddUsuario(CrearUsuarioDto dto)
         {
             var entity = new UsuarioEntity
             {
-                Nombre = dto.nombre,
-                Correo = dto.correo,
-                Telefono = dto.telefono,
-                IdRol = dto.idRol,
-                Clave = dto.clave,
-                EsActivo = true,
-                FechaRegistro = DateTime.Now
+                nombre = dto.nombre,
+                correo = dto.correo,
+                telefono = dto.telefono,
+                idRol = dto.idRol,
+                clave = dto.clave,
+                esActivo = true,
+                fechaRegistro = DateTime.Now
             };
 
             await Usuario.AddAsync(entity);
@@ -99,6 +100,57 @@ namespace API_SisVenta.Repositories
             await SaveChangesAsync();
             return true;
         }
+
+        //Metodos Rol
+        // MÉTODOS ROL ----------------------------------------
+
+        public async Task<RolEntity?> GetRol(int id) =>
+            await Rol.FirstOrDefaultAsync(x => x.idRol == id);
+
+        public async Task<RolEntity> AddRol(CreaRolDto dto)
+        {
+            var entity = new RolEntity
+            {
+                descripcion = dto.descripcion,
+                esActivo = dto.esActivo,
+                fechaRegistro = DateTime.Now
+            };
+
+            await Rol.AddAsync(entity);
+            await SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<RolDto?> UpdateRol(RolDto dto)
+        {
+            var entity = await Rol.FirstOrDefaultAsync(x => x.idRol == dto.idRol);
+
+            if (entity == null)
+                return null;
+
+            entity.descripcion = dto.descripcion;
+            entity.esActivo = dto.esActivo;
+
+            Rol.Update(entity);
+            await SaveChangesAsync();
+
+            return entity.ToDto();
+        }
+
+        public async Task<bool> DeleteRol(int id)
+        {
+            var entity = await Rol.FirstOrDefaultAsync(x => x.idRol == id);
+
+            if (entity == null)
+                return false;
+
+            Rol.Remove(entity);
+            await SaveChangesAsync();
+
+            return true;
+        }
+
     }
 
     // ENTIDADES ----------------------------------------
@@ -129,10 +181,10 @@ namespace API_SisVenta.Repositories
 
     public class RolEntity
     {
-        public int IdRol { get; set; }
-        public string Descripcion { get; set; }
-        public bool? EsActivo { get; set; }
-        public DateTime? FechaRegistro { get; set; }
+        public int idRol { get; set; }
+        public string descripcion { get; set; }
+        public bool? esActivo { get; set; }
+        public DateTime? fechaRegistro { get; set; }
 
         public ICollection<UsuarioEntity> Usuarios { get; set; }
 
@@ -140,30 +192,39 @@ namespace API_SisVenta.Repositories
         {
             return new RolDto
             {
-                IdRol = this.IdRol,
-                Descripcion = this.Descripcion,
-                EsActivo = this.EsActivo
+                idRol = this.idRol,
+                descripcion = this.descripcion,
+                esActivo = this.esActivo,
             };
         }
     }
 
     public class UsuarioEntity
     {
-        public int IdUsuario { get; set; }
-        public string Nombre { get; set; }
-        public string Correo { get; set; }
-        public string Telefono { get; set; }
-        public int? IdRol { get; set; }
-        public string UrlFoto { get; set; }
-        public string NombreFoto { get; set; }
-        public string Clave { get; set; }
-        public bool? EsActivo { get; set; }
-        public DateTime? FechaRegistro { get; set; }
+        public int idUsuario { get; set; }
+        public string nombre { get; set; }
+        public string correo { get; set; }
+        public string telefono { get; set; }
+        public int? idRol { get; set; }
+        public string urlFoto { get; set; }
+        public string nombreFoto { get; set; }
+        public string clave { get; set; }
+        public bool? esActivo { get; set; }
+        public DateTime? fechaRegistro { get; set; }
 
         public RolEntity Rol { get; set; }
 
+        public UsuarioDto ToDto()
+        {
+            return new UsuarioDto
+            {
+                idUsuario = this.idUsuario,
+                nombre = this.nombre,
+                correo = this.correo,
+                telefono = this.telefono,
+                idRol = this.idRol
+            };
+        }
     }
 
-
 }
-
