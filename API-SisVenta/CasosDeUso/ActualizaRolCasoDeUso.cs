dda7f5b6bfaf9c0e57f715bdae167b590ebc57c5
Artie20 <149128422+Artie20@users.noreply.github.com>
2025-11-20 17:10:22 -0600

    Add ActualizaRolCasoDeUso for role updates
    Implemented the ActualizaRolCasoDeUso class to update role details in the database.

--- API-SisVenta/CasosDeUso/ActualizaRolCasoDeUso.cs
+++ API-SisVenta/CasosDeUso/ActualizaRolCasoDeUso.cs
@@ -1,6 +1,38 @@
-﻿namespace API_SisVenta.CasosDeUso
+using API_SisVenta.Dtos;
+using API_SisVenta.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_SisVenta.CasosDeUso
 {
-    public class ActualizaRolCasoDeUso
+    public interface IActualizaRolCasoDeUso
     {
+        Task<RolDto?> Execute(RolDto rol);
     }
+
+    public class ActualizaRolCasoDeUso : IActualizaRolCasoDeUso
+    {
+        private readonly DBVENTAbakContext _db;
+
+        public ActualizaRolCasoDeUso(DBVENTAbakContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<RolDto?> Execute(RolDto rol)
+        {
+            var entity = await _db.Rol.FirstOrDefaultAsync(x => x.idRol == rol.idRol);
+            if (entity == null)
+            {
+                return null;
+            }
+
+            entity.descripcion = rol.descripcion;
+            entity.esActivo = rol.esActivo;
+
+            await _db.SaveChangesAsync();
+
+            return entity.ToDto();
+        }
+    }
+
 }
