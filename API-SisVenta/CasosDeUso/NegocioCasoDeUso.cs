34b3418adb92b73112845078c2990ae12722731b
Artie20 <149128422+Artie20@users.noreply.github.com>
2025-11-21 12:46:36 -0600

    Add files via upload

---
+++ API-SisVenta/CasosDeUso/NegocioCasoDeUso.cs
@@ -0,0 +1,41 @@
+﻿using API_SisVenta.Dtos;
+using API_SisVenta.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_SisVenta.CasosDeUso
+{
+   public interface IActualizaNegocioCasoDeUso
+   {
+       Task<NegocioDto?> Execute(NegocioDto negocio);
+   }
+    public class ActualizaNegocioCasoDeUso : IActualizaNegocioCasoDeUso
+    {
+        private readonly DBVENTAbakContext _DBventabakContext;
+       public ActualizaNegocioCasoDeUso(DBVENTAbakContext DBventabakContext)
+       {
+           _DBventabakContext = DBventabakContext;
+       }
+        public async Task<NegocioDto?> Execute(NegocioDto negocio)
+        {
+            var entity = await _DBventabakContext.GetNegocio(negocio.idNegocio);
+
+            if (entity == null)
+                return null;
+
+            entity.urlLogo = negocio.urlLogo;
+            entity.nombreLogo = negocio.nombreLogo;
+            entity.numeroDocumento = negocio.numeroDocumento;
+            entity.nombre = negocio.nombre;
+            entity.correo = negocio.correo;
+            entity.direccion = negocio.direccion;
+            entity.telefono = negocio.telefono; // CORREGIDO
+            entity.porcentajeImpuesto = negocio.porcentajeImpuesto;
+            entity.simboloMoneda = negocio.simboloMoneda;
+
+            await _DBventabakContext.ActualizarNegocio(entity);
+
+            return entity.ToDto();
+        }
+
+    }
+}
