97c4340563337b64320e17aab1d65dc3a312a671
Artie20 <149128422+Artie20@users.noreply.github.com>
2025-11-21 16:03:29 -0600

    Add files via upload

---
+++ API-SisVenta/Dtos/Venta.cs
@@ -0,0 +1,17 @@
+﻿namespace API_SisVenta.Dtos
+{
+    public class VentaDto
+    {
+        public int idVenta { get; set; }
+        public string numeroVenta { get; set; }
+        public int? idTipoDocumentoVenta { get; set; }
+        public int? idUsuario { get; set; }
+        public decimal? subTotal { get; set; }
+        public decimal? impuestoTotal { get; set; }
+        public decimal? total { get; set; }
+        public DateTime? fechaRegistro { get; set; }
+        public int? idCliente { get; set; }
+        public decimal? descuento { get; set; }
+    }
+}
+
