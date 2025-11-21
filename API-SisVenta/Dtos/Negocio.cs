d6e8b285e12e4e37fd3d5259eef11470a1999d84
Artie20 <149128422+Artie20@users.noreply.github.com>
2025-11-21 12:48:07 -0600

    Add files via upload

---
+++ API-SisVenta/Dtos/Negocio.cs
@@ -0,0 +1,14 @@
+﻿public class NegocioDto
+{
+    public int idNegocio { get; set; }
+    public string urlLogo { get; set; }
+    public string nombreLogo { get; set; }
+    public string numeroDocumento { get; set; }
+    public string nombre { get; set; }
+    public string correo { get; set; }
+    public string direccion { get; set; }
+    public string telefono { get; set; }
+    public decimal porcentajeImpuesto { get; set; } // ✅ CORRECTO
+    public string simboloMoneda { get; set; }
+}
+
