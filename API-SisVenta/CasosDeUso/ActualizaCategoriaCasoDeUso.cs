40a8e2c5020891c7bf9cd5f5c34154d0a27607b7
Artie20 <149128422+Artie20@users.noreply.github.com>
2025-11-21 16:00:51 -0600

    Add files via upload

---
+++ API-SisVenta/CasosDeUso/ActualizaCategoriaCasoDeUso.cs
@@ -0,0 +1,38 @@
+﻿using API_SisVenta.Dtos;
+using API_SisVenta.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_SisVenta.CasosDeUso
+{
+    public interface IActualizaCategoriaCasoDeUso
+    {
+        Task<CategoriaDto?> Execute(CategoriaDto categoria);
+    }
+
+    public class ActualizaCategoriaCasoDeUso : IActualizaCategoriaCasoDeUso
+    {
+        private readonly DBVENTAbakContext _context;
+
+        public ActualizaCategoriaCasoDeUso(DBVENTAbakContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<CategoriaDto?> Execute(CategoriaDto dto)
+        {
+            var entity = await _context.Categoria
+                                      .FirstOrDefaultAsync(c => c.idCategoria == dto.idCategoria);
+
+            if (entity == null)
+                return null;
+
+            entity.descripcion = dto.descripcion;
+            entity.esActivo = dto.esActivo;
+
+            await _context.SaveChangesAsync();
+
+            return entity.ToDto();
+        }
+    }
+}
+
