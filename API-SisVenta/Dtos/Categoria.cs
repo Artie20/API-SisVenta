92b8106e697fedaf37945ac38e968edcd12c97c1
Artie20 <149128422+Artie20@users.noreply.github.com>
2025-11-21 16:03:03 -0600

    Add files via upload

---
+++ API-SisVenta/Dtos/Categoria.cs
@@ -0,0 +1,18 @@
+﻿namespace API_SisVenta.Dtos
+{
+    public class CategoriaDto
+    {
+        public int idCategoria { get; set; }
+        public string? descripcion { get; set; }
+        public bool? esActivo { get; set; }
+        public DateTime? fechaRegistro { get; set; }
+    }
+
+
+    public class CreaCategoriaDto
+    {
+        public string descripcion { get; set; }
+        public bool? esActivo { get; set; }
+    }
+}
+
