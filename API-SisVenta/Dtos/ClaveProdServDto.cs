a1be96e17288d125f321e668500affef62f8d4cf
Damaris26445 <142068365+Damaris26445@users.noreply.github.com>
2025-11-22 18:21:48 -0600

    Add ClaveProdServDto class for product service keys

---
+++ API-SisVenta/Dtos/ClaveProdServDto.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace API_SisVenta.Dtos
+{
+    public class ClaveProdServDto
+    {
+        public int idClaveProdServ_SAT { get; set; }
+        public string? c_ClaveProdServ_SAT { get; set; }
+        public string? descripcion { get; set; }
+        public DateTime? fechaRegistro { get; set; }
+    }
+}
