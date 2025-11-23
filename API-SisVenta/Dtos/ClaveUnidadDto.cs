8f1f239d87bdb5e07c036ef746e53f4d50b7f71c
Damaris26445 <142068365+Damaris26445@users.noreply.github.com>
2025-11-22 18:26:11 -0600

    Add ClaveUnidadDto class for unit key representation

---
+++ API-SisVenta/Dtos/ClaveUnidadDto.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace API_SisVenta.Dtos
+{
+    public class ClaveUnidadDto
+    {
+        public int idClaveUnidad_SAT { get; set; }
+        public string? c_ClaveUnidad_SAT { get; set; }
+        public string? descripcion { get; set; }
+        public DateTime? fechaRegistro { get; set; }
+    }
+}
